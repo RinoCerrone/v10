72bfe7b52dcd3db68471eb8f2e68fefedb338e06
Matko <tkoma999@gmail.com>
2023-01-16 20:17:24 +0100

    Test commit

--- test/test.cpp
+++ test/test.cpp
@@ -22,7 +22,7 @@ namespace all_tests
 			std::stringstream ss("14 -78 22");
 			std::vector<int> v;
 			// TODO: read values from input stream into vector
-
+            copy(istream_iterator<int> in(ss),istream_iterator<int> ends,back_inserter(v));
 			Assert::AreEqual(3ull, v.size());
 			Assert::AreEqual(14, v[0]);
 			Assert::AreEqual(-78, v[1]);
@@ -73,47 +73,47 @@ namespace all_tests
 		TEST_METHOD(test_04a)
 		{
 			std::stringstream ss("1.5 2.5 3.5");
-			auto res = // TODO: sum of all values in input stream
+			//auto res = // TODO: sum of all values in input stream
 			Assert::AreEqual(7.5, res);
 		}
 		TEST_METHOD(test_04b)
 		{
 			std::vector<std::string> v{ "V", "S", "I", "T", "E", "!" };
-			auto res = // TODO: concatenated string with additional prefix 
+			//auto res = // TODO: concatenated string with additional prefix 
 			Assert::AreEqual("GO VSITE!", res.c_str());
 		}
 		TEST_METHOD(test_04c)
 		{
 			struct person { std::string name; int age; };
 			std::vector<person> v{ {"Pero", 33}, {"Iva", 25} };
-			auto total_age = // TODO: sum of all ages
+			//auto total_age = // TODO: sum of all ages
 			Assert::AreEqual(58, total_age);
 		}
 
 		TEST_METHOD(test_05a)
 		{
 			std::vector<int> v{ -5, 8, 11, 0, -9, 77, -4 };
-			auto number_of_negative = // TODO: 
+			//auto number_of_negative = // TODO: 
 			Assert::AreEqual(3ll, number_of_negative);
 		}
 		TEST_METHOD(test_05b)
 		{
 			std::vector<double> v{ 1.5, 8, -11.23, 0, 1e10, 1e10, 1e10, 0, 99 };
-			auto number_of_invalid = // TODO: 
+			//auto number_of_invalid = // TODO: 
 			Assert::AreEqual(3ll, number_of_invalid);
 		}
 		TEST_METHOD(test_05c)
 		{
 			struct point { int x, y; };
 			std::vector<point> v{ {1,1}, {-5,3}, {2,2}, {-7,-6}, {9,-4} };
-			auto number_in_first_quadrant = // TODO: 
+			//auto number_in_first_quadrant = // TODO: 
 			Assert::AreEqual(2ll, number_in_first_quadrant);
 		}
 
 		TEST_METHOD(test_06)
 		{
 			std::vector<int> v{ 33, 16, 24, 41, 25, 19, 9 };
-			auto first_prime = // TODO: 
+			//auto first_prime = // TODO: 
 			Assert::AreEqual(41, first_prime);
 		}
 		TEST_METHOD(test_07a)
@@ -174,16 +174,16 @@ namespace all_tests
 		TEST_METHOD(test_11)
 		{
 			std::vector<double> v{ 11, 0.5, -97.23, -23.11, 48.78, 22.96, -77 };
-			auto smallest_value = // TODO: 
+			//auto smallest_value = // TODO: 
 			Assert::AreEqual(-97.23, smallest_value);
-			auto largest_value = // TODO: 
+			//auto largest_value = // TODO: 
 			Assert::AreEqual(48.78, largest_value);
 		}
 		TEST_METHOD(test_12)
 		{
 			std::vector<int> atp_points{ 8445, 7480, 6220, 5300, 5285 };
 			// the most interesting match is the one with the smallest difference
-			auto smallest_difference = // TODO: 
+			//auto smallest_difference = // TODO: 
 			Assert::AreEqual(15, smallest_difference);
 		}
 	};
